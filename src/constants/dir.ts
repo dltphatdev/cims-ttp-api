f625837ad06bf38c5be94fe1f9fce54e42055743
thanhphatdev301 <dltphat301@gmail.com>
2025-07-08 08:48:09 +0000

    update code 8.7.2025

--- src/constants/dir.ts
+++ src/constants/dir.ts
@@ -1,13 +1,13 @@
 import { isProduction } from '@/constants/config'
 import path from 'path'
 
-export const UPLOAD_IMAGE_DIR = path.resolve(isProduction ? '/home/cimsprod/uploads/image' : 'uploads/image')
-export const UPLOAD_FILE_DIR = path.resolve(isProduction ? '/home/cimsprod/uploads/file' : 'uploads/file')
-export const UPLOAD_FILES_DIR = path.resolve(isProduction ? '/home/cimsprod/uploads/files' : 'uploads/files')
-export const UPLOAD_IMAGES_DIR = path.resolve(isProduction ? '/home/cimsprod/uploads/images' : 'uploads/images')
+export const UPLOAD_IMAGE_DIR = path.resolve(isProduction ? '/home/administrator/uploads/image' : 'uploads/image')
+export const UPLOAD_FILE_DIR = path.resolve(isProduction ? '/home/administrator/uploads/file' : 'uploads/file')
+export const UPLOAD_FILES_DIR = path.resolve(isProduction ? '/home/administrator/uploads/files' : 'uploads/files')
+export const UPLOAD_IMAGES_DIR = path.resolve(isProduction ? '/home/administrator/uploads/images' : 'uploads/images')
 export const UPLOAD_IMAGE_TEMP_DIR = path.resolve(
-  isProduction ? '/home/cimsprod/uploads/image/temp' : 'uploads/image/temp'
+  isProduction ? '/home/administrator/uploads/image/temp' : 'uploads/image/temp'
 )
 export const UPLOAD_IMAGES_TEMP_DIR = path.resolve(
-  isProduction ? '/home/cimsprod/uploads/images/temp' : 'uploads/images/temp'
+  isProduction ? '/home/administrator/uploads/images/temp' : 'uploads/images/temp'
 )
